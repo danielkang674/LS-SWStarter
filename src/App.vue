fb0fa26e71a8f7cc4b8c395528c3f5a54358f5dd
Daniel Kang <danielkang674@gmail.com>
2018-11-09 20:25:36 -0600

    Create basic search container

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <header>
+      <h1 class="SWStarter">SWStarter</h1>
+    </header>
     <router-view/>
   </div>
 </template>
@@ -12,5 +15,25 @@
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+header {
+  width: 1440px;
+  height: 50px;
+  box-shadow: 0 2px 0 0 #dadada;
+  background-color: #ffffff;
+}
+
+.SWStarter {
+  width: 98px;
+  height: 22px;
+  font-family: Montserrat;
+  font-size: 18px;
+  font-weight: bold;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #0ab463;
 }
 </style>
